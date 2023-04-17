e5a97d96b76f402eba0ea52853a46631088a3d4e
s561417 <123023199+s561417@users.noreply.github.com>
2023-04-17 11:03:47 -0500

    gfd

--- controllers/guns.js
+++ controllers/guns.js
@@ -1,5 +1,5 @@
 var guns = require('../models/guns');
-// List of all Costumes
+// List of all gunss
 exports.guns_list = async function(req, res) {
     try{
         theGuns = await guns.find();
@@ -9,18 +9,26 @@ exports.guns_list = async function(req, res) {
         res.send(`{"error": ${err}}`);
      } 
 };
-// for a specific Costume.
-exports.guns_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: guns detail: ' + req.params.id);
-};
-// Handle Costume create on POST.
+// for a specific guns.
+// for a specific guns.
+exports.guns_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await guns.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+// Handle guns create on POST.
 exports.guns_create_post = async function(req, res) {
     console.log(req.body)
     let document = new guns();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"guns_type":"goat", "cost":12, "size":"large"}
     document.guns_color = req.body.color;
     document.guns_range = req.body.range;
     document.guns_cost = req.body.cost;
@@ -33,13 +41,29 @@ exports.guns_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 };
-// Handle Costume delete form on DELETE.
+// Handle guns delete form on DELETE.
 exports.guns_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: guns delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
-exports.guns_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: guns update PUT' + req.params.id);
+// Handle guns update form on PUT.
+exports.guns_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await guns.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.guns_color)
+ toUpdate.guns_color = req.body.guns_color;
+ if(req.body.guns_range) toUpdate.guns_range = req.body.guns_range;
+ if(req.body.guns_cost) toUpdate.guns_cost = req.body.guns_cost;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
 };
 
 // VIEWS
