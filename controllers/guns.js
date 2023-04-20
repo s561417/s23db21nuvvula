3b413c45d8512e14e002937bf0cf9a64a16a27c8
s561417 <123023199+s561417@users.noreply.github.com>
2023-04-20 00:37:18 -0500

    gvcg

--- controllers/guns.js
+++ controllers/guns.js
@@ -41,10 +41,20 @@ exports.guns_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 };
-// Handle guns delete form on DELETE.
-exports.guns_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: guns delete DELETE ' + req.params.id);
-};
+
+// Handle Costume delete on DELETE.
+exports.guns_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await guns.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
 // Handle guns update form on PUT.
 exports.guns_update_put = async function(req, res) {
  console.log(`update on id ${req.params.id} with body
@@ -78,3 +88,59 @@ exports.guns_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// Handle a show one view with id specified by query
+exports.guns_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await guns.findById( req.query.id)
+    res.render('gunsdetail',
+   { title: 'guns Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.guns_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('gunscreate', { title: 'guns Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.guns_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await guns.findById(req.query.id)
+    res.render('gunsupdate', { title: 'guns Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle a delete one view with id from query
+exports.guns_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await guns.findById(req.query.id)
+    res.render('gunsdelete', { title: 'guns Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/guns.js
+++ routes/guns.js
@@ -9,4 +9,17 @@ router.get('/', function(req, res, next) {
 
 router.get('/', guns_controlers.guns_view_all_Page)
 
+/* GET detail guns page */
+router.get('/detail', guns_controlers.guns_view_one_Page);
+
+/* GET create guns page */
+router.get('/create', guns_controlers.guns_create_Page);
+
+/* GET create update page */
+router.get('/update', guns_controlers.guns_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', guns_controlers.guns_delete_Page);
+
 module.exports = router;
+
