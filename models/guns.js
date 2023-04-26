7f58acee53eca4aa591d3cb74cdcb8abf72024c4
s561417 <123023199+s561417@users.noreply.github.com>
2023-04-26 12:51:10 -0500

    dcj chdjcb

--- models/guns.js
+++ models/guns.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const gunsSchema = mongoose.Schema({
-    guns_color: String,
-    guns_range: String,
-    guns_cost: String
+    guns_color: {type: String, minlength: 1, maxlength: 50},
+    guns_range: {type: String, minlength: 1, maxlength: 50},
+    guns_cost: {type: String, minlength: 1, maxlength: 50}
 })
 module.exports = mongoose.model("Guns",gunsSchema)
--- routes/guns.js
+++ routes/guns.js
@@ -7,19 +7,29 @@ router.get('/', function(req, res, next) {
   res.render('guns', { title: 'Search Results guns' });
 });*/
 
+
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
+
 router.get('/', guns_controlers.guns_view_all_Page)
 
 /* GET detail guns page */
-router.get('/detail', guns_controlers.guns_view_one_Page);
+router.get('/detail',secured, guns_controlers.guns_view_one_Page);
 
 /* GET create guns page */
-router.get('/create', guns_controlers.guns_create_Page);
+router.get('/create',secured, guns_controlers.guns_create_Page);
 
 /* GET create update page */
-router.get('/update', guns_controlers.guns_update_Page);
+router.get('/update',secured, guns_controlers.guns_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', guns_controlers.guns_delete_Page);
+router.get('/delete',secured, guns_controlers.guns_delete_Page);
 
 module.exports = router;
 
